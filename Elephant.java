0e222bc3a6a917e78b854ca327849f1de00a01c2
five-high-seas <117937319+five-high-seas@users.noreply.github.com>
2022-11-24 13:12:01 -0500

    Elephant Class in Greenfoot

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author (Vincent) 
+ * @version (version1 | 11/24/2022)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(2);
+    }
+}
