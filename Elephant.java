accd50558c77113a9ff75d6446678bf65f56466f
five-high-seas <117937319+five-high-seas@users.noreply.github.com>
2022-12-06 12:54:52 -0500

    Changed elephant, apple and background images

--- Elephant.java
+++ Elephant.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -41,6 +43,8 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.spawnApple();
             world.increaseScore();
+            
+            elephantSound.play();
         }
     }
 }
