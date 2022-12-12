e0dbd99bee7a14b550f9b9a16e3b2995ce7bbc28
five-high-seas <117937319+five-high-seas@users.noreply.github.com>
2022-12-12 12:51:54 -0500

    Added variable to adjust elephant animation

--- Elephant.java
+++ Elephant.java
@@ -13,7 +13,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[14];
     
     String facing = "right";
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant()
     {
         for(int i = 0; i < idleRight.length; i++)
@@ -30,12 +30,21 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(50, 50);
         }
+        
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
+        
     }
     
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 42)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
